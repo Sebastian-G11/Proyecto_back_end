5dc939b06c7fa6dd95eff9358c1dc633c010f238
Sebastian-G11 <sebastian.gomez.reyes11@gmail.com>
2025-09-03 15:17:16 -0400

    build: modificacion de settings
    se puede levantar correctamente el servidor al aplicar las configuraciones de "INSTALLED _APPS" en settings.py

--- gestorProyecto/gestorProyecto/settings.py
+++ gestorProyecto/gestorProyecto/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
-
+import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -37,6 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'acciones',
+    'actividades',
+    'autenticacion',
+    'dimensiones',
+    'solicitud_materiales',
+    'sub_actividades',
+    'usuarios'
 ]
 
 MIDDLEWARE = [
@@ -54,7 +61,7 @@ ROOT_URLCONF = 'gestorProyecto.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -116,6 +123,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
