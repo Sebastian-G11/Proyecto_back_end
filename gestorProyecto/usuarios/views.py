8c9e0821db43358085a5af921cac0f5edc157fa9
Sebastian-G11 <sebastian.gomez.reyes11@gmail.com>
2025-09-04 19:57:13 -0400

    feat: se agrego el login
    se agrego el login el cual contiene 2 usuarios los cuales pueden iniciar sesion (aun es diseño base)

--- gestorProyecto/usuarios/views.py
+++ gestorProyecto/usuarios/views.py
@@ -1,5 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect
 
 # Create your views here.
+
+usuarios = [
+    {'nombre' : 'sebas',  'contrasena' : '1234'},
+     {'nombre' : 'pepe', 'contrasena' : '123'}
+    
+]
+
 def display(request):
-    return render(request, "")
+    mensaje = ""
+    if request.method == "POST":
+        user = request.POST.get("nombre", "")
+        pwd = request.POST.get("contrasena", "")
+
+
+        encontrado = any(u['nombre'] == user and u['contrasena'] == pwd for u in usuarios)
+
+        if encontrado:
+            return redirect("/acciones/")  # redirige a la raíz que ya muestra renderTemplate
+        else:
+            mensaje = "Usuario o contraseña incorrecta"
+
+    return render(request, "usuario/login.html", {"mensaje": mensaje})
