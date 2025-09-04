63b27d7b94c7ad668d1ebe5a828abfc2d3c1ba4e
benjamon <benjafu78@gmail.com>
2025-09-04 12:36:44 -0400

    feat: agregar navbar

--- gestorProyecto/acciones/views.py
+++ gestorProyecto/acciones/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def display(request):
-    return render(request, 'acciones/prueba1.html')
+    return render(request, 'acciones/lista_acciones.html')
