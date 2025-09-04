dc8da66eb28d60d213bd3137c4f63211e309bbd7
benjamon <benjafu78@gmail.com>
2025-09-04 12:51:11 -0400

    feat: agregar active a navbar según ruta

--- gestorProyecto/actividades/views.py
+++ gestorProyecto/actividades/views.py
@@ -2,4 +2,4 @@ from django.shortcuts import render
 
 # Create your views here.
 def prueba2(request):
-    return render(request, 'actividades/prueba2.html')
+    return render(request, 'actividades/lista_actividades.html')
