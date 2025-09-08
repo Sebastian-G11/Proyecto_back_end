a7d2887b3719f5088ae4621de18459cfcddb70b1
benjamon <benjafu78@gmail.com>
2025-09-08 11:11:07 -0300

    feat: lista de dimensiones con crud

--- gestorProyecto/dimensiones/views.py
+++ gestorProyecto/dimensiones/views.py
@@ -1,5 +1,16 @@
 from django.shortcuts import render
 
+dimensiones = [
+    {
+        "dimension_id": 1,
+        "nombre": "Gestión de Capacitaciones"
+    },
+    {
+        "dimension_id": 2,
+        "nombre": "Interacción con el Cliente"
+    }
+]
+
 # Create your views here.
 def display_dimensiones(request):
-    return render(request, "dimensiones/prueba1.html")
+    return render(request, "dimensiones/lista_dimensiones.html", { "dimensiones": dimensiones })
