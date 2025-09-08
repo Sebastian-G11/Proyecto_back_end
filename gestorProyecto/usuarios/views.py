e496fa5e55fa0c0ef27c14762eeee5efd6b54501
benjamon <benjafu78@gmail.com>
2025-09-08 10:49:26 -0300

    feat: lista de usuarios con crud

--- gestorProyecto/usuarios/views.py
+++ gestorProyecto/usuarios/views.py
@@ -1,24 +1,11 @@
 from django.shortcuts import render,redirect
 
 # Create your views here.
-
-usuarios = [
-    {'nombre' : 'sebas',  'contrasena' : '1234'},
-     {'nombre' : 'pepe', 'contrasena' : '123'}
-]
-
 def display(request):
-    mensaje = ""
-    if request.method == "POST":
-        user = request.POST.get("nombre", "")
-        pwd = request.POST.get("contrasena", "")
-
-
-        encontrado = any(u['nombre'] == user and u['contrasena'] == pwd for u in usuarios)
-
-        if encontrado:
-            return redirect("/acciones/")  # redirige a la raíz que ya muestra renderTemplate
-        else:
-            mensaje = "Usuario o contraseña incorrecta"
-
-    return render(request, "usuario/login.html", {"mensaje": mensaje})
+    usuarios = [
+        {"nombre": "Juan", "apellido": "Pérez", "email": "juan.perez@example.com", "rol": "Administrador"},
+        {"nombre": "María", "apellido": "Gómez", "email": "maria.gomez@example.com", "rol": "Usuario"},
+        {"nombre": "Carlos", "apellido": "López", "email": "carlos.lopez@example.com", "rol": "Invitado"},
+        {"nombre": "Ana", "apellido": "Torres", "email": "ana.torres@example.com", "rol": "Usuario"},
+    ]
+    return render(request, "usuarios/lista_usuarios.html", {"usuarios": usuarios})
