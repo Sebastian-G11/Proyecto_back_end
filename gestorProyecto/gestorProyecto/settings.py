11d99399f120de795a20ad46c51e94de221bfbf1
Sebastian-G11 <sebastian.gomez.reyes11@gmail.com>
2025-10-06 16:12:47 -0300

    Conexion a base de datos

--- gestorProyecto/gestorProyecto/settings.py
+++ gestorProyecto/gestorProyecto/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 
 from pathlib import Path
 import os
+import pymysql
+pymysql.install_as_MySQLdb()
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -87,10 +89,14 @@ WSGI_APPLICATION = 'gestorProyecto.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'django_db',
+        'USER': 'root',
+        'PASSWORD': '220935043',
+        'HOST': '127.0.0.1',
+        'PORT': '3307',
+}}
+
 
 
 # Password validation
