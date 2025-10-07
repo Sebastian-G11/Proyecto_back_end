83bbcd2fde7e80833154a9b61816a2695a810f8c
benjamon <benjafu78@gmail.com>
2025-10-07 19:04:52 -0300

    refact: aplicar uso de servicios desde vista

--- gestorProyecto/solicitud_materiales/repositorio/repository.py
+++ gestorProyecto/solicitud_materiales/repositorio/repository.py
@@ -4,14 +4,15 @@ from solicitud_materiales.models import SolicitudMaterial
 class SolicitudRepository(SolicitudRepositoryI):
     solicitudes_model = SolicitudMaterial
 
+
     def create_solicitud(self, data):
         return self.solicitudes_model.objects.create(**data)
     
     def get_solicitudes(self):
         return self.solicitudes_model.objects.all()
-    
-    def update_solicitud(self, id):
-        return super().update_solicitud(id)
+
+    def update_solicitud(self, id, data):
+        return self.solicitudes_model.objects.filter(solicitud_id=id).update(**data)
 
     def delete_solicitud(self, id):
-        return super().delete_solicitud(id)
+        return self.solicitudes_model.objects.filter(solicitud_id=id).delete()
--- gestorProyecto/solicitud_materiales/repositorio/repository_interface.py
+++ gestorProyecto/solicitud_materiales/repositorio/repository_interface.py
@@ -1,16 +1,17 @@
 from abc import ABC, abstractmethod
 
 class SolicitudRepositoryI(ABC):
+    
     @abstractmethod
     def get_solicitudes(self):
         pass
     
     @abstractmethod
-    def create_solicitud(self):
+    def create_solicitud(self, data):
         pass
     
     @abstractmethod
-    def update_solicitud(self, id) -> bool:
+    def update_solicitud(self, id, data) -> bool:
         pass
     
     @abstractmethod
