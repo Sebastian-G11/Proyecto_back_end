fc6d45c2c320ff830acdb87e5c36d5e0493a2f90
Benjamin <benjafu78@gmail.com>
2025-11-03 11:10:17 -0300

    feat: relaciones

--- gestorProyecto/acciones/models.py
+++ gestorProyecto/acciones/models.py
@@ -1,16 +1,28 @@
 from django.db import models
+from dimensiones.models import Dimensiones
+from usuarios.models import Usuarios
+
+class Estados(models.Model):
+    estado_id = models.AutoField(primary_key=True)
+    nombre = models.CharField(max_length=50, unique=True)
 
 # Create your models here.
 class Accion(models.Model):
     accion_id = models.AutoField(primary_key=True)
+    dimension = models.ForeignKey(Dimensiones, related_name='acciones', on_delete=models.PROTECT, default=None)
     nombre = models.CharField(max_length=100)
+    responsable = models.ForeignKey(Usuarios, related_name='acciones_responsable', on_delete=models.PROTECT, default=None)
     presupuesto_anual = models.IntegerField()
     presupuesto_reajustado = models.IntegerField(null=True, blank=True)
     descripcion = models.TextField()
     fecha_creacion = models.DateField(auto_now_add=True)
     fecha_modificacion = models.DateField(auto_now=True)
+    estado = models.ForeignKey(Estados, related_name='acciones', on_delete=models.PROTECT, default=1)
 
 class VerificacionAccion(models.Model):
     verificacion_id = models.AutoField(primary_key=True)
+    accion = models.ForeignKey(Accion, related_name='verificaciones', on_delete=models.CASCADE, default=None)
     nombre = models.CharField(max_length=40)
     url = models.URLField()
+
+ 
--- gestorProyecto/solicitud_materiales/models.py
+++ gestorProyecto/solicitud_materiales/models.py
@@ -1,8 +1,10 @@
 from django.db import models
+from actividades.models import Actividad
 
 # Create your models here.
 class SolicitudMaterial(models.Model):
     solicitud_id = models.AutoField(primary_key=True)
+    actividad_id = models.ForeignKey(Actividad, related_name='solicitudes_materiales', on_delete=models.PROTECT, default=None)
     materiales_solicitados = models.CharField(max_length=50)
     numero_orden = models.CharField(max_length=15, unique=True)
     valor_esperado = models.IntegerField()
